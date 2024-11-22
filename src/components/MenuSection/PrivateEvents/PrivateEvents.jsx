dd55e779c51147700579d4a7823f793b7815e18e
fasih581 <fasihedakkudi@gamil.com>
2024-11-23 00:04:12 +0530

    second commit

--- src/components/MenuSection/PrivateEvents/PrivateEvents.jsx
+++ src/components/MenuSection/PrivateEvents/PrivateEvents.jsx
@@ -8,18 +8,20 @@ import Img from '../../../assets/barimg.jpg'
 
 function PrivateEvents() {
     return (
-        <div className={Css.container}>
-            <Navbar />
-            <div className={Css.Container}>
-                <dir className={Css.TextBox}>
-                    <h4>PRIVATE DINING & EVENTS</h4>
-                    <p>Lyra provides an ideal waterfront setting for corporate enents and special occasions.A particular
-                        highlight is our private wine dining room,whic seats up to eight peaple and can host bespoke wine dinners curated by
-                        our head chef and sommenier
-                    </p>
-                </dir>
-                <div className={Css.ImgBox}>
-                    <img src={Img} alt="" />
+        <>
+            <div className={Css.container}>
+                <Navbar />
+                <div className={Css.Container}>
+                    <dir className={Css.TextBox}>
+                        <h4>PRIVATE DINING & EVENTS</h4>
+                        <p>Lyra provides an ideal waterfront setting for corporate enents and special occasions.A particular
+                            highlight is our private wine dining room,whic seats up to eight peaple and can host bespoke wine dinners curated by
+                            our head chef and sommenier
+                        </p>
+                    </dir>
+                    <div className={Css.ImgBox}>
+                        <img src={Img} alt="" />
+                    </div>
                 </div>
             </div>
             <div className={Css.Footercontainer}>
@@ -27,7 +29,7 @@ function PrivateEvents() {
                     <FooterSec />
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
